3d84d715dacb60332ce74a6324b7e436a17151ca
Todd Wolfson <todd@twolfson.com>
2013-12-03 15:39:35 -0800

    Implemented server

--- test/hai-node-summit_test.js
+++ test/hai-node-summit_test.js
@@ -1,11 +1,27 @@
-var hai_node_summit = require('../');
+var assert = require('assert');
+var request = require('request');
+var Server = require('../');
 
+// TODO: Show off perceptual diffs if we have time =)
 describe('hai-node-summit', function () {
   before(function () {
-
+    this.app = new Server(9002);
+  });
+  after(function (done) {
+    this.app.close(done);
   });
 
-  it('', function () {
-
+  describe('replying to a request', function () {
+    before(function (done) {
+      var that = this;
+      request('http://localhost:9002/', function (err, res, body) {
+        that.body = body;
+        done(err);
+      });
+    });
+    it('returns "Hello World!"', function () {
+      assert.strictEqual(this.body, 'Hello World!\n');
+    });
   });
 });
+
